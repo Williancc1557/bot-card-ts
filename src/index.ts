111196ef4a3a540212d0f792b9d920c6bdab7715
Willian Cavalcanti Coelho <minegt1500@gmail.com>
2022-01-17 08:10:04 -0300

    bug

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ import { client } from "./client";
 import * as dotenv from "dotenv"; // Used to get data in .env
 dotenv.config();
 
+console.log("test");
+
 client.login(process.env.TOKEN);
--- src/infra/db.ts
+++ src/infra/db.ts
@@ -1,6 +1,4 @@
 import { Pool } from "pg";
-import * as dotenv from "dotenv"; // Used to get data in .env
-dotenv.config();
 
 export const db = new Pool({
     connectionString: process.env.DATABASE_URL,
